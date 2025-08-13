d52693bfe2835cea7546dd5027a7db8cabdb23c1
he2497114-222 <2497114777@qq.com>
2025-08-13 09:52:01 +0800

    消除释放寄存器的注释

--- src/riscv/codegenerator.cpp
+++ src/riscv/codegenerator.cpp
@@ -113,7 +113,7 @@ void CodeGenerator::generateAssignment(const IRInstruction& inst) {
         if (srcReg != destReg) {
             emit("  mv " + destReg + ", " + srcReg);
         }
-        // if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
+        if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
     }
 
 }
@@ -148,8 +148,8 @@ void CodeGenerator::generateArithmetic(const IRInstruction& inst) {
 
     emit("  " + op + " " + rd + ", " + rs1 + ", " + rs2);
 
-    // if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
-    // if(inst.arg2->type == OperandType::TEMP) regAlloc.freeReg(inst.arg2->toString());
+    if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
+    if(inst.arg2->type == OperandType::TEMP) regAlloc.freeReg(inst.arg2->toString());
 }
 
 void CodeGenerator::generateControlFlow(const IRInstruction& inst) {
@@ -168,7 +168,7 @@ void CodeGenerator::generateControlFlow(const IRInstruction& inst) {
             std::string condReg = getRegorLoad(inst.arg1);;
             std::string validLabel = generateValidLabel(inst.label);
             emit("  bnez " + condReg + ", " + validLabel);
-            // if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
+            if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
             break;
         }
         default: break;
@@ -284,7 +284,7 @@ void CodeGenerator::generateFunctionCall(const IRInstruction& inst) {
             emit("  mv " + argReg + ", " + srcReg);
         }
         emit("  sw " + argReg + ", " + std::to_string(stackParamOffset) + "(sp)");
-        // if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(arg);
+        if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(arg);
     }
 
     resetParamStrings();
@@ -332,7 +332,7 @@ void CodeGenerator::generateReturn(const IRInstruction& inst) {
         else{
             std::string retReg = getRegorLoad(inst.arg1);
             emit("  mv a0, " + retReg);
-            // regAlloc.freeReg(inst.arg1->toString());
+            regAlloc.freeReg(inst.arg1->toString());
         }
     }
     emit("  j " + currentFuncExitLabel);  // 跳转到函数统一退出标签
@@ -372,8 +372,8 @@ void CodeGenerator::generateComparison(const IRInstruction& inst) {
             throw std::runtime_error("Unsupported comparison operator");
     }
 
-    // if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
-    // if(inst.arg2->type == OperandType::TEMP) regAlloc.freeReg(inst.arg2->toString());
+    if(inst.arg1->type == OperandType::TEMP) regAlloc.freeReg(inst.arg1->toString());
+    if(inst.arg2->type == OperandType::TEMP) regAlloc.freeReg(inst.arg2->toString());
     
 }
 
